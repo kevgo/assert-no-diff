38a37adf3da7ae8107676e43695e40a39d102574
Kevin Goslar <kevin.goslar@gmail.com>
2020-04-01 11:37:40 -0500

    Update dependencies

--- test/json-test.ts
+++ test/json-test.ts
@@ -5,37 +5,37 @@ import * as assertNoDiff from "../src/index"
 
 suite("assertNoDiff.json")
 
-test("matching data", function() {
+test("matching data", function () {
   const data = { firstName: "Jean-Luc", lastName: "Picard" }
   assertNoDiff.json(data, data)
 })
 
-test("mismatching data", function() {
+test("mismatching data", function () {
   const obj1 = { firstName: "Jean-Luc", lastName: "Picard" }
   const obj2 = { firstName: "Captain", lastName: "Picard" }
   const expected = chalk`mismatching objects:
 
 {grey \{\n}{red   "firstName": "Jean-Luc",\n}{green   "firstName": "Captain",\n}{grey   "lastName": "Picard"\n\}}`
-  assert.throws(function() {
+  assert.throws(function () {
     assertNoDiff.json(obj2, obj1)
   }, new Error(expected))
 })
 
-test("no expected value", function() {
-  assert.throws(function() {
+test("no expected value", function () {
+  assert.throws(function () {
     // @ts-ignore
     assertNoDiff.json("foo")
   }, new Error("AssertNoDiff: expected value not provided"))
 })
 
-test("no actual value", function() {
-  assert.throws(function() {
+test("no actual value", function () {
+  assert.throws(function () {
     // @ts-ignore
     assertNoDiff.json()
   }, new Error("AssertNoDiff: actual value not provided"))
 })
 
-test("custom error message", function() {
+test("custom error message", function () {
   try {
     assertNoDiff.json({ a: 1 }, { a: 2 }, "custom message")
   } catch (e) {
@@ -46,6 +46,6 @@ test("custom error message", function() {
   throw new Error("assertNoDiff.json didn't throw")
 })
 
-test("diffing empty objects", function() {
+test("diffing empty objects", function () {
   assertNoDiff.json({}, {})
 })
