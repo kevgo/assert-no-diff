07ce8d8dd2ecb4ad7706837143dd6a9c77d78a6a
Kevin Goslar <kevin.goslar@gmail.com>
2020-10-27 15:03:07 -0500

    Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  rules: {
+    "@typescript-eslint/no-use-before-define": ["error", "nofunc"],
+    "@typescript-eslint/member-delimiter-style": [
+      "error",
+      { multiline: { delimiter: "none" } },
+    ],
+    "@typescript-eslint/no-explicit-any": ["off"],
+    "@typescript-eslint/ban-ts-comment": ["off"]
+  },
+}
--- Makefile
+++ Makefile
@@ -22,7 +22,7 @@ help:   # prints all make targets
 
 lint:   # lints all files
 	node_modules/.bin/tsc --noEmit
-	node_modules/.bin/tslint --project .
+	${CURDIR}/node_modules/.bin/eslint -c .eslintrc.js --ext .ts .
 	node_modules/.bin/prettier -c "src/*.ts"
 	node_modules/.bin/prettier -c "test/*.ts"
 	node_modules/.bin/prettier -c "*.md"
