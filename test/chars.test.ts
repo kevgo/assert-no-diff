7371d7b302140c9abdc8b49a2c412c66f818e382
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-12 09:54:37 -0500

    chars.test.ts json.test.ts trimmed-lines.test.ts words-with-space.test.ts

--- test/chars.test.ts
+++ test/chars.test.ts
@@ -4,53 +4,54 @@ import chalk from "chalk"
 import stripAnsi from "strip-ansi"
 import * as assertNoDiff from "../src/index"
 
-suite("assertNoDiff.chars()")
+suite("assertNoDiff.chars()", function () {
 
-test("matching data", function () {
-  const data = "Jean-Luc Picard"
-  assertNoDiff.chars(data, data)
-})
+  test("matching data", function () {
+    const data = "Jean-Luc Picard"
+    assertNoDiff.chars(data, data)
+  })
 
-test("mismatching data", function () {
-  const obj1 = "Jean-Luc Picard"
-  const obj2 = "Captain Picard"
-  const expected = chalk`mismatching strings:
+  test("mismatching data", function () {
+    const obj1 = "Jean-Luc Picard"
+    const obj2 = "Captain Picard"
+    const expected = chalk`mismatching strings:
 
 {red Je}{green C}{grey a}{green ptai}{grey n}{red -Luc}{grey  Picard}`
-  assert.throws(
-    function () {
-      assertNoDiff.chars(obj2, obj1)
-    },
-    new Error(expected),
-    "should throw color-coded diff"
-  )
-})
+    assert.throws(
+      function () {
+        assertNoDiff.chars(obj2, obj1)
+      },
+      new Error(expected),
+      "should throw color-coded diff"
+    )
+  })
 
-test("no expected value", function () {
-  assert.throws(function () {
-    // @ts-ignore: intentional call without second argument
-    assertNoDiff.chars("foo")
-  }, new Error("AssertNoDiff: expected value not provided"))
-})
+  test("no expected value", function () {
+    assert.throws(function () {
+      // @ts-ignore: intentional call without second argument
+      assertNoDiff.chars("foo")
+    }, new Error("AssertNoDiff: expected value not provided"))
+  })
 
-test("no actual value", function () {
-  assert.throws(function () {
-    // @ts-ignore: intentional call without arguments
-    assertNoDiff.chars()
-  }, new Error("AssertNoDiff: actual value not provided"))
-})
+  test("no actual value", function () {
+    assert.throws(function () {
+      // @ts-ignore: intentional call without arguments
+      assertNoDiff.chars()
+    }, new Error("AssertNoDiff: actual value not provided"))
+  })
 
-test("providing a custom message", function () {
-  try {
-    assertNoDiff.chars("one", "two", "custom message")
-  } catch (e) {
-    const stripped = stripAnsi(e.message)
-    assert.equal(stripped, "custom message:\n\ntwone")
-    return
-  }
-  throw new Error("assertNoDiff.chars didn't throw")
-})
+  test("providing a custom message", function () {
+    try {
+      assertNoDiff.chars("one", "two", "custom message")
+    } catch (e) {
+      const stripped = stripAnsi(e.message)
+      assert.equal(stripped, "custom message:\n\ntwone")
+      return
+    }
+    throw new Error("assertNoDiff.chars didn't throw")
+  })
 
-test("diffing against empty strings", function () {
-  assertNoDiff.chars("", "")
+  test("diffing against empty strings", function () {
+    assertNoDiff.chars("", "")
+  })
 })
