70500ee939a6de711a3b89b1ce1563dc54fa8cb9
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-12 09:53:46 -0500

    chars.test.ts json.test.ts trimmed-lines.test.ts words-with-space.test.ts

--- test/trimmed-lines.test.ts
+++ test/trimmed-lines.test.ts
@@ -1,4 +1,5 @@
-import { strict as assert } from "assert"
+import assert from "node:assert/strict"
+import { suite, test } from "node:test"
 import chalk from "chalk"
 import stripAnsi from "strip-ansi"
 import * as assertNoDiff from "../src/index"
